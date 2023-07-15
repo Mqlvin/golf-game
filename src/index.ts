cc3963d0bbc9efca2d841bbd3613b2c379102e3d
Mqlvin <pricklydotcactus@gmail.com>
2023-07-15 17:14:51 +0100

    Show example asset manager impl

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@ const app = new PIXI.Application({
     view: document.getElementById('game-canvas') as HTMLCanvasElement,
 });
 
+let assetManager = new AssetManager();
+assetManager.loadAllAssets().then(() => {
+    const dude = new PIXI.Sprite(assetManager.getAsset("golf", "background_blue"));
+    dude.anchor.set(0.5);
+
+    // move the sprite to the center of the screen
+    dude.x = app.screen.width / 2;
+    dude.y = app.screen.height / 2;
+
+    app.stage.addChild(dude);
+});
