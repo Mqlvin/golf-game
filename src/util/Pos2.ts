8faa4b4e271c6cf36d54409acffd2919e027be93
Mqlvin <pricklydotcactus@gmail.com>
2023-07-17 19:36:25 +0100

    Add utility position class

---
+++ src/util/Pos2.ts
@@ -0,0 +1,33 @@
+/*
+ * A placeholder class for a 2D position.
+ */
+
+export class Pos2 {
+    private _x: number;
+    private _y: number;
+
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+
+    public get x(): number {
+        return this._x;
+    }
+
+    public get y(): number {
+        return this._y;
+    }
+
+    public set y(newY: number) {
+        this._y = newY;
+    }
+
+    public set x(newX: number) {
+        this._x = newX;
+    }
+
+    toString(): string {
+        return "[Pos2 object] x:" + (this._x) + " y:" + (this._y);
+    }
+}
