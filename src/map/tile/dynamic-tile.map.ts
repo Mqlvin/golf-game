d2990e39aa5ecf6540462c3b1a0daa6d8bc814bd
Mqlvin <pricklydotcactus@gmail.com>
2023-07-23 20:19:13 +0100

    Make dynamic tiles abstract

--- src/map/tile/dynamic-tile.map.ts
+++ src/map/tile/dynamic-tile.map.ts
@@ -4,6 +4,6 @@ import { MapTile } from "./tile.map";
  * A placeholder class holding fields for dynamic tiles on the map, such as collidable tiles.
  */
 
-export class DynamicTile extends MapTile {
-    
+export abstract class DynamicTile extends MapTile {
+    abstract update(): void;
 }
