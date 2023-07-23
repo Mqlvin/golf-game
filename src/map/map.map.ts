a54c6c85acf2064a3aa34293facb11eed7c22497
Mqlvin <pricklydotcactus@gmail.com>
2023-07-23 20:22:42 +0100

    Add related game level methods

--- src/map/map.map.ts
+++ src/map/map.map.ts
@@ -1,20 +1,61 @@
-import { Container, DisplayObject } from "pixi.js";
-import { DynamicTile } from "./tile/dynamic-tile.map";
-import { StaticTile } from "./tile/static-tile.map";
+import { clampInt } from "../util/math.util";
+import { GameLevel } from "./level.map";
 
 export class GameMap {
-    private _staticTiles: StaticTile[];
-    private _dynamicTiles: DynamicTile[];
+    private levels: GameLevel[];
+    private activeLevelIndex: number;
 
-    constructor(_staticTiles: StaticTile[], _dynamicTiles: DynamicTile[]) {
-        this._staticTiles = _staticTiles;
-        this._dynamicTiles = _dynamicTiles;
+    constructor(levels: GameLevel[], startingLevelIndex?: 0) { // optional parameter
+        this.levels = levels;
+        this.activeLevelIndex = (startingLevelIndex == undefined ? 0 : startingLevelIndex); // if param passed, make it param
     }
 
-    constructScene(stage: Container<DisplayObject>): void {
-        this._staticTiles.forEach((tile: StaticTile) => {
-            console.log("adding tile: " + tile)
-            stage.addChild(tile.sprite);
-        });
+    /*
+     * Public method for incrementing level.
+     */
+    incrementLevel() {
+        this.activeLevelIndex++;
+    }
+
+    /*
+     * Public method for decrementing level.
+     */
+    decrementLevel(clamp?: boolean) {
+        this.activeLevelIndex--;
+
+        // if should clamp index
+        if(!clamp) this.activeLevelIndex = clampInt(this.activeLevelIndex, 0, this.getLevelCount() - 1);
+    }
+
+    /*
+     * Public method for adjusting level. Remove if unused.
+     */
+    setLevel(index: number) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+    getLevelCount(): number {
+        return this.levels.length;
     }   
+
+
+
+
+    private updateIntervalLevel() {
+
+    }
+
+    private checkWin(): boolean {
+        //     if activeLevelIndex outside positive index range of levels
+        return this.activeLevelIndex >= this.getLevelCount();
+    }
 }
